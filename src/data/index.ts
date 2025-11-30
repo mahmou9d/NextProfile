d3b3da035d0716db4d64909da4acf7701fccb1b9
mahm9oud <eatshouse@gmail.com>
2025-11-30 18:40:30 +0200

    up

--- src/data/index.ts
+++ src/data/index.ts
@@ -299,8 +299,8 @@ export const workExperience = [
     },
 ];
 
-export const socialMedia = [
-    { id: 1, img: "/git.svg" },
-    { id: 2, img: "/twit.svg" },
-    { id: 3, img: "/link.svg" },
-];
+// export const socialMedia = [
+//     { id: 1, img: "/git.svg" },
+//     { id: 2, img: "/twit.svg" },
+//     { id: 3, img: "/link.svg" },
+// ];
