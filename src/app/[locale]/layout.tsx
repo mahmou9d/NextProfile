09193097e443c9721f970629110a14c90b5415b8
mahmou9d <mohnud0987@gmail.com>
2026-02-01 22:54:36 +0200

    updata

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -39,13 +39,23 @@ export async function generateMetadata({
     description: t.description,
     icons: {
       icon: [
-        { url: "/Webvitas.png" },
-        { url: "/Webvitas.png", sizes: "32x32", type: "image/png" },
-        { url: "/Webvitas.png", sizes: "192x192", type: "image/png" },
-        { url: "/Webvitas.png", sizes: "512x512", type: "image/png" },
+        { url: "/favicon.ico" },
+        { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+        {
+          url: "/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          url: "/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
       ],
-      apple: [{ url: "/Webvitas.png", sizes: "180x180", type: "image/png" }],
-      shortcut: "/Webvitas.png",
+      apple: [
+        { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+      ],
+      shortcut: "/favicon.ico",
     },
     keywords: isArabic
       ? [
